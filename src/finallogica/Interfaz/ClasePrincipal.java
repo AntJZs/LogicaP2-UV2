9d45c64d50f91f228a304b2ebda91fedbe1c0a3b
Antonio De León <antjz@duck.com>
2024-05-05 16:45:24 -0500

    Creada una clase principal, que carga la interfaz.

---
+++ src/finallogica/Interfaz/ClasePrincipal.java
@@ -0,0 +1,15 @@
+package finallogica.Interfaz;
+
+/**
+ *
+ * @author AntJZ
+ * @comment main class of the package
+ */
+public class ClasePrincipal {
+
+    public static void main(String[] args) {
+        Inicio init = new Inicio();
+        init.setVisible(true);
+    }
+
+}
